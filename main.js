8147eec1c88f811deec5a697405c543b72d91562
Sophie Sargsyan <sophiesargsyan@Sophies-MacBook-Air.local>
2024-02-20 21:54:56 +0400

    added winning combos

--- main.js
+++ main.js
@@ -26,6 +26,20 @@ function playerMove(board, player){
 
 let currentBoard = playerMove(board, player1);
 
+// Winning combos
+let winningCombos = [
+    [board[0],board[1],board[2]],
+    [board[3],board[4],board[5]],
+    [board[6],board[7],board[8]],
+    [board[0],board[3],board[6]],
+    [board[1],board[4],board[7]],
+    [board[2],board[5],board[8]],
+    [board[0],board[4],board[8]],
+    [board[2],board[4],board[6]]
+]
+
+
+
 
 
 
