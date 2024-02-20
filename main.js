f19401b7b54bf446037f262a56c3ba721dacc064
Sophie Sargsyan <sophiesargsyan@Sophies-MacBook-Air.local>
2024-02-20 21:24:19 +0400

    player choise x or o

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let player1 = prompt('Choose x or o:').toUpperCase();
+let player2 = (player1 === 'X')? 'O':'X';
+let step = Number(prompt('Enter board-number for your move:'));
+let board = ['1','2','3','4','5','6','7','8','9', '10'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
