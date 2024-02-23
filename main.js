96afe5130efafc7446572da44485d1127d663cdf
Sophie Sargsyan <sophiesargsyan@Sophies-MacBook-Air.local>
2024-02-23 18:51:13 +0400

    added function which checks wrong moves

--- main.js
+++ main.js
@@ -34,7 +34,14 @@ function player1Input() {
     let step;
     do {
         step = +prompt('Enter value for your move: ');
-    } while (step < 1 || step > 10 || isNaN(step) || board[step - 1] === 'X' || board[step - 1] === 'O');
+        if (step < 1 || step > 10 || isNaN(step) || board[step - 1] === 'X' || board[step - 1] === 'O') {
+            alert("Invalid move!");
+            return -1;
+        } else if (player2Steps.includes(step.toString())) {
+            alert("Number already taken by Player!");
+            return -1;
+        }
+    } while (isNaN(step));
     return step;
 }
 
@@ -43,6 +50,9 @@ let player2StepsCount = 0;
 
 function player1Move(board, player) {
     let step = player1Input();
+    if (step === -1) {
+        return null; 
+    }
     player1Steps.push(step.toString());
     player1StepsCount++;
     board[step - 1] = player;
@@ -68,7 +78,11 @@ function randomStep() {
 }
 
 while (true) {
-    player1Move(board, player1);
+    let player1MoveResult = player1Move(board, player1);
+    if (!player1MoveResult) {
+        alert("Player made an invalid move. Game over.");
+        break; 
+    }
     if (checkWin(player1Steps)) {
         alert(`Congratulations! You win.`);
         break;
